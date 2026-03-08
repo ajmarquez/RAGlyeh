e80b8db5758c7617484342f927f76871015f170f
Abelardo Marquez <abelardo@hey.com>
2026-03-08 22:02:45 +0100

    Updates main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.api.routes.health import router as health_router
+from app.api.routes.stories import router as stories_router
 
 app = FastAPI()
 app.include_router(health_router)
+app.include_router(stories_router)
 
 
 @app.get("/")
